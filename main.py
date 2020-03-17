180eae3895e4ff2c24dfe2a81e745595cdae73ff
seongwon <abc7988se@gmail.com>
2020-03-17 14:52:32 +0900

    Add regex classifier

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from os import rename, walk
 from pyexcel_xlsx import get_data
+from re import match
 import config
 
 data = get_data(config.EXCEL_PATH)
+test_regex = r"\d{4}-\d{2}-\d{2} \d{2} \d{2} \d{2}"
 date = {}
 
 for sheet in data:
     for row in data[sheet]:
-        if len(row) > 11:
-            date[row[10]] = row[10]
-
-for file in walk(config.DIRECTORY_PATH):
-    if date[file] != None:
-        rename(file, date[file])
+        if len(row) > 1 and match(test_regex, row[-1]) is not None:
+            dates = row[-1].split("\n")
+            for dt in dates:
+                date[dt] = row[0]
+# for file in walk(config.DIRECTORY_PATH):
+#     if date[file] != None:
+#         rename(file, f"(date[file])" + file)
 
 # for file in walk(config.DIRECTORY_PATH):
 #     print(file)
